d6d0fecce8a947b764b783bab4579db7ae50685d
Harikant Sharma <harikantsharmag@gmail.com>
2025-05-30 17:40:58 -0400

    Delete twoweingroupnew directory

--- twoweingroupnew/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // <-- add this
-  images: {
-    domains: ['images.unsplash.com'], // your custom domains
-  },
-};
-
-export default nextConfig;
